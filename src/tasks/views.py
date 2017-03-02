2ed5801720e3f534da24d3064ee7db29b81e6e47
Jose Antonio Maldonado <jamaldonadojimenez@gmail.com>
2017-03-02 23:03:37 +0100

    enlazamos urls en plantillas con url names

--- src/tasks/views.py
+++ src/tasks/views.py
@@ -1,3 +1,5 @@
+from django.http import HttpResponse
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
 from tasks.models import Task
@@ -10,10 +12,36 @@ def tasks_list(request):
     :return: HttpResponse
     """
     # Recuperar todas las tareas de la BBDD
-    tasks = Task.objects.all()
+    tasks = Task.objects.select_related("owner", "assignee").all()
 
     # Devolver la respuesta
     context = {
         'task_objects': tasks
     }
     return render(request, 'tasks/list.html', context)
+
+
+def tasks_detail(request, task_pk):
+    """
+    Recupera una tarea de la BBDD y la pinta con una plantilla
+    :param request: HttpRequest
+    :param task_pk: Primary key de la tarea a recuperar
+    :return: HttpResponse
+    """
+
+    # Recuperar la tarea
+    # Opción 1
+    try:
+        task = Task.objects.get(pk=task_pk)
+    except Task.DoesNotExist:
+        return HttpResponseNotFound("La tarea que buscas no existe")
+    except Task.MultipleObjectsReturned:
+        return HttpResponse("Existen varias tareas con ese identificador", status=300)
+
+    # Preparar el contexto
+    context = {
+        'task': task
+    }
+
+    # Renderizar la plantilla
+    return render(request, 'tasks/detail.html', context)
