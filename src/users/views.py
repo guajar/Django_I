d6c6c17dc186c59d8e9701b1c9803e77dc87dd6c
Jose Antonio Maldonado <jamaldonadojimenez@gmail.com>
2017-03-06 22:33:02 +0100

    Convertimos la función de login en una vista basada en clase

--- src/users/views.py
+++ src/users/views.py
@@ -1,15 +1,25 @@
 from django.contrib.auth import authenticate, login as django_login, logout as django_logout
 from django.shortcuts import render, redirect
+from django.views import View
 
 
-def login(request):
-    """
-    Hace login de un usuario
-    :param request: HttpRequest
-    :return: HttpResponse
-    """
-    context = dict()    # Iniciamos diccionario
-    if request.method == 'POST':
+class LoginView(View):
+
+    def get(self, request):
+        """
+        Presenta el formulario de login de usuario
+        :param request: HttpRequest
+        :return: HttpResponse
+        """
+        return render(request, 'login.html')
+
+    def post(self, request):
+        """
+        Hace login de un usuario
+        :param request: HttpRequest
+        :return: HttpResponse
+        """
+        context = dict()  # Iniciamos diccionario
         username = request.POST.get("usr")
         password = request.POST.get("pwd")
         user = authenticate(username=username, password=password)
@@ -22,7 +32,7 @@ def login(request):
             # Usuario no autenticado
             context["error"] = "Wrong username or password"
 
-    return render(request, 'login.html', context)
+        return render(request, 'login.html', context)
 
 
 def logout(request):
