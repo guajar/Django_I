3cab2db16d9a822fa4fc4ffed77e271a5e512098
Jose Antonio Maldonado <jamaldonadojimenez@gmail.com>
2017-03-07 23:10:12 +0100

    Endpoint de actualizacion de usuarios

--- src/users/api.py
+++ src/users/api.py
@@ -52,3 +52,30 @@ class UserDetailAPI(APIView):
         user = get_object_or_404(User, pk=pk)
         serializer = UserSerializer(user)
         return Response(serializer.data)
+
+    def put(self, request, pk):
+        """
+        Updates an User with the given data
+        :param request: HttpRequest
+        :param pk: User primary key
+        :return: Response
+        """
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+
+
+    def delete(self, request, pk):
+        """
+        Delete an user
+        :param request: HttpRequest
+        :param pk: User primary key
+        :return: Response
+        """
+        user = get_object_or_404(User, pk=pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
