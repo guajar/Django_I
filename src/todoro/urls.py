b9ffb334eb39fb34bb6abb6a9785f7518c40dd88
Jose Antonio Maldonado <jamaldonadojimenez@gmail.com>
2017-03-07 22:23:21 +0100

    Creamos endpoint de detalle de un usuario

--- src/todoro/urls.py
+++ src/todoro/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from tasks.views import tasks_list, tasks_detail, NewTaskView
+from users.api import UsersAPI, UserDetailAPI
 from users.views import LoginView, logout
 
 urlpatterns = [
@@ -24,5 +25,9 @@ urlpatterns = [
     url(r'^tasks/(?P<task_pk>[0-9]+)$', tasks_detail, name="tasks_detail"),
     url(r'^tasks/new$', NewTaskView.as_view(), name="tasks_new"),
     url(r'^login$', LoginView.as_view(), name="login"),
-    url(r'^logout$', logout, name='logout')
+    url(r'^logout$', logout, name='logout'),
+
+    # API
+    url(r'^api/1.0/users/$', UsersAPI.as_view(), name="users_api"),
+    url(r'^api/1.0/users/(?P<pk>[0-9]+)$', UserDetailAPI.as_view(), name="user_detail_api"),
 ]
---
+++ src/users/api.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+
+from django.contrib.auth.models import User
+from rest_framework.generics import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+from users.serializers import UserSerializer
+
+
+class UsersAPI(APIView):
+    """
+    List (GET) and creates (POST) users
+    """
+    def get(self, request):
+        """
+        Returns a list of the system users
+        :param request: HttpRequest
+        :return: Response
+        """
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)   # many para decir que es una lista, no solo 1
+        return Response(serializer.data)
+
+    def post(self, request):
+        """
+        Creates a user
+        :param request:
+        :return:
+        """
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserDetailAPI(APIView):
+    """
+    User detail (GET) update user (PUT), delete user (DELETE)
+    """
+
+    def get(self, request, pk):
+        """
+        Returns a requested user
+        :param request:
+        :param pk: user primary key
+        :return: Response
+        """
+        user = get_object_or_404(User, pk=pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
