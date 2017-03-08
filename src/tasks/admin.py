821e66ac61788d6a4ba14bf4375ea21568ee9118
Jose Antonio Maldonado <jamaldonadojimenez@gmail.com>
2017-03-08 19:58:03 +0100

    Personalizamos el administrador de Django

--- src/tasks/admin.py
+++ src/tasks/admin.py
@@ -1,5 +1,27 @@
 from django.contrib import admin
 from tasks.models import Task
 
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
 
-admin.site.register(Task)
+    list_display = ('name', 'status', 'owner', 'assignee')
+    list_filter = ('status', 'owner', 'assignee')
+    search_fields = ('name', 'description')
+    readonly_fields = ('created_at', 'modified_at')
+
+    fieldsets = (
+        (None, {
+            "fields": ("name", "description")
+        }),    # La coma es para que sepa que es una tupla (Problemas con las tuplas)
+        ("Owner and assignee", {
+            "fields": ("owner", "assignee"),
+            "description": "Please, do not overclock your partners"
+        }),
+        ("Meta", {
+            "fields": ("status", "time_estimated", "deadline"),
+            "classes": ("collapse",)
+        }),
+        ("Dates", {
+            "fields": ("created_at", "modified_at")
+        })
+    )
