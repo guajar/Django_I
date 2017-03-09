7a7ec2a6192eb9eb2038731e7d334ce2deed9bb7
Jose Antonio Maldonado <jamaldonadojimenez@gmail.com>
2017-03-09 22:24:40 +0100

    Protegemos la API task con sguridad

--- src/tasks/api.py
+++ src/tasks/api.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from tasks.models import Task
@@ -13,7 +14,7 @@ class TasksAPI(ListCreateAPIView):
     Lists (GET) and creates (POST) Tasks
     """
     queryset = Task.objects.all()
-    serializer_class = TaskSerializer
+    permission_classes = (IsAuthenticated,)
 
     def get_serializer_class(self):
         return TasksListSerializer if self.request.method == "GET" else TaskSerializer
@@ -25,3 +26,4 @@ class TaskDetailAPI(RetrieveUpdateDestroyAPIView):
     """
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = (IsAuthenticated,)
